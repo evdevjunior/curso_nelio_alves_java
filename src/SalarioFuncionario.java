39cb0f216128d76a17186c8dbfd74544c356a26c
evdevjunior <evdevjunior@hotmail.com>
2024-05-30 15:24:38 -0300

    calculo do salario pelas horas trabalhadas

---
+++ src/SalarioFuncionario.java
@@ -0,0 +1,32 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/* Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por 
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas 
+decimais. */
+
+public class SalarioFuncionario {
+    
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int funcionario_id;
+        double horasTrabalhada;
+        double valorHora;
+
+        Scanner sc = new Scanner(System.in);
+
+        funcionario_id = sc.nextInt();
+        horasTrabalhada = sc.nextDouble();
+        valorHora = sc.nextDouble();
+        double salario = (double) (horasTrabalhada * valorHora); 
+
+        System.out.println("ID_FUNCIONARIO : " + funcionario_id);
+        System.out.println("SALARIO : " + salario);
+        sc.close();
+        
+
+    }
+}
