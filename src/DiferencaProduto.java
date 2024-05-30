3b3c4969193c4377460adf7a6dd2d97394407b45
evdevjunior <evdevjunior@hotmail.com>
2024-05-30 15:04:18 -0300

    adidionado a descrição do exercicio

--- src/DiferencaProduto.java
+++ src/DiferencaProduto.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+/* Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). */
+
 public class DiferencaProduto {
     
     public static void main(String[] args) {
