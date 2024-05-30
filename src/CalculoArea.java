11701e6c252a79460b8d9655d180741749ed9dc7
evdevjunior <evdevjunior@hotmail.com>
2024-05-30 14:30:28 -0300

    calculo da area de um circulo

---
+++ src/CalculoArea.java
@@ -0,0 +1,28 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2
+Considere o valor de π = 3.14159 */
+
+public class CalculoArea {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double A;
+        double raio;
+        raio = sc.nextDouble();
+        A = Math.PI*raio*raio;
+        
+        System.out.printf("A = %.4f", A);
+        sc.close();
+        
+    }
+
+}
